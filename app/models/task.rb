d865d921f4fd4c9e3fcf9f9750d7a22cf7165c84
Quang Minh <minhtruong20002@gmail.com>
2022-05-26 17:10:37 +0700

    Add function login, CRUD: project & task, add boostrap

---
+++ app/models/task.rb
@@ -0,0 +1,23 @@
+class Task < ApplicationRecord
+  belongs_to :project
+
+  validates :status, inclusion: { in: ['not-started', 'in-progress', 'complete'] }
+
+  STATUS_OPTIONS = [
+    ['Not started', 'not-started'],
+    ['In progress', 'in-progress'],
+    ['Complete', 'complete']
+  ]
+
+  def badge_color
+    case status
+    when 'not-started'
+      'secondary'
+    when 'in-progress'
+      'info'
+    when 'complete'
+      'success'
+      end
+  end
+
+end
